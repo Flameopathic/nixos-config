b59d323790a2e657c959c5b6593ef539c2c2f7b3
Flameopathic <flameopathic@gmail.com>
2024-04-05 11:04:26 -0400

    enabling avahi in an attempt to get local dns to resolve

--- prof/nixos/ssh-server.nix
+++ prof/nixos/ssh-server.nix
@@ -1,15 +1,17 @@
 { config, pkgs, inputs, ... }: {
-	services.openssh = {
-		enable = true;
-		# best practice is to require public-key authentication
-		settings.PasswordAuthentication = false;
-		settings.KbdInteractiveAuthentication = false;
+	services = {
+	  avahi = {
+			enable = true;
+			nssmdns = true;
+		};
+	  openssh = {
+			enable = true;
+			# best practice is to require public-key authentication
+			settings.PasswordAuthentication = false;
+			settings.KbdInteractiveAuthentication = false;
+		};
 	};
-	users.users."flame".openssh.authorizedKeys.keys = [
+	users.users.root.openssh.authorizedKeys.keys = [
 		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC38crYgufmRowdoWdCfMZxF0uwl2xcFuNLjnN8tspUP flame"
-	];
-  networking.firewall.allowedTCPPorts = [
-		22
-		80
 	];
 }
