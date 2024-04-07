5841b73799f64b06ad166749731e5411814aba27
Flameopathic <flameopathic@gmail.com>
2024-04-07 13:14:23 -0400

    allow fnix2 to connect to servnix

--- prof/nixos/ssh-server.nix
+++ prof/nixos/ssh-server.nix
@@ -9,5 +9,6 @@
 	};
 	users.users.flame.openssh.authorizedKeys.keys = [
 		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC38crYgufmRowdoWdCfMZxF0uwl2xcFuNLjnN8tspUP flame"
+		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILc5oDe4Uk3ZgEIENiQL7gZIv1FLFh37iPs9zxrrizfP flame"
 	];
 }
