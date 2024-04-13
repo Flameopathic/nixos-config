196c55d71e4e74029b15b2ef306d49ed0addddf3
Flameopathic <flameopathic@gmail.com>
2024-04-13 10:13:48 -0400

    changed ssh keys to allow for remote builds

--- prof/nixos/ssh-server.nix
+++ prof/nixos/ssh-server.nix
@@ -11,4 +11,8 @@
 		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC38crYgufmRowdoWdCfMZxF0uwl2xcFuNLjnN8tspUP flame"
 		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILc5oDe4Uk3ZgEIENiQL7gZIv1FLFh37iPs9zxrrizfP flame"
 	];
+	users.users.root.openssh.authorizedKeys.keys = [
+		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC38crYgufmRowdoWdCfMZxF0uwl2xcFuNLjnN8tspUP flame"
+		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILc5oDe4Uk3ZgEIENiQL7gZIv1FLFh37iPs9zxrrizfP flame"
+	];
 }
