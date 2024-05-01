ddd0258932c806eec25fe3fe168e132591dc4c33
Flameopathic <flameopathic@gmail.com>
2024-04-30 21:26:16 -0400

    sddm theming

--- prof/nixos/ui.nix
+++ prof/nixos/ui.nix
@@ -1,9 +1,11 @@
 { config, pkgs, inputs, ... }: {
+  environment.systemPackages = [ pkgs.where-is-my-sddm-theme ];
   services.xserver = {
     enable = true;
     displayManager.sddm = {
       enable = true;
       wayland.enable = true;
+      theme = "where_is_my_sddm_theme";
     };
   };
 
