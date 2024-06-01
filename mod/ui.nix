4163f43c002933f5a83e05c4bfa5c2d1eaa4ad40
Flameopathic <flameopathic@gmail.com>
2024-06-01 08:34:11 -0400

    attempt to fix sddm theming, failed

--- mod/ui.nix
+++ mod/ui.nix
@@ -10,13 +10,13 @@ in {
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = [ pkgs.where-is-my-sddm-theme ];
+    environment.systemPackages = [ pkgs.catppuccin-sddm ]; # themeing not working
     services = {
       xserver.enable = true;
       displayManager.sddm = {
         enable = true;
         wayland.enable = true;
-        theme = "where_is_my_sddm_theme";
+        theme = "catppuccin";
       };
     };
 
