0cab478ff264259ced5bb334089ad68c4db40741
Flameopathic <flameopathic@gmail.com>
2024-06-18 12:21:04 -0400

    more vscode settings

--- mod-hm/vscode.nix
+++ mod-hm/vscode.nix
@@ -20,6 +20,7 @@ in {
         "workbench.sideBar.location" = "right";
         "explorer.confirmDragAndDrop" = false;
         "remoteHub.commitDirectlyWarning" = "off";
+        "git.enableSmartCommit" = true;
         "workbench.colorTheme" = "Rosé Pine Dawn (no italics)";
       };
     };
