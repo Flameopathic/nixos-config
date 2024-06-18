ae31b1a9f56313bfe1c9120ae984d10e85ebd74c
Flameopathic <flameopathic@gmail.com>
2024-06-18 19:48:36 -0400

    minor adjustment to last commit

--- mod-hm/hyprland.nix
+++ mod-hm/hyprland.nix
@@ -150,6 +150,7 @@ in {
                   in [
         	    "$mod, ${ws}, workspace, ${toString (x + 1)}"
         	    "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+              "$mod SHIFT CTRL, ${ws}, movetoworkspacesilent, ${toString (x + 1)}"
         	  ]
           ) 10)
         );
