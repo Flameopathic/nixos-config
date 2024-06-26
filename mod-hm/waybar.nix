06190cb0451508bc4bf36d199324c2a27f561afc
Flameopathic <flameopathic@gmail.com>
2024-06-26 15:49:12 -0400

    remove unnecessary waybar module

--- mod-hm/waybar.nix
+++ mod-hm/waybar.nix
@@ -57,23 +57,6 @@
           format-disconnected = "Disconnected ⚠";
           format-alt = "{ifname}: {ipaddr}/{cidr}";
         };
-        pulseaudio = {
-          format = "{volume}% {icon} {format_source}";
-          format-bluetooth = "{volume}% {icon} {format_source}";
-          format-bluetooth-muted = " {icon} {format_source}";
-          format-source = "{volume}% ";
-          format-source-muted = "";
-          format-icons = {
-            headphone = "";
-            hands-free = "";
-            headset = "";
-            phone = "";
-            portable = "";
-            car = "";
-            default = ["" "" ""];
-          };
-          onclick = "pavucontrol";
-        };
       };
     };
 
