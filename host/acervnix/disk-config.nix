cf6bc0e8c4ea1d9759f6d681a3cde34211958717
Flameopathic <flameopathic@gmail.com>
2024-06-26 12:41:12 -0400

    acervnix improvements

--- host/acervnix/disk-config.nix
+++ host/acervnix/disk-config.nix
@@ -18,7 +18,7 @@
               };
             };
             root = {
-              end = "-1G";
+              end = "-8G";
               content = {
                 type = "filesystem";
                 format = "ext4";
--- host/acervnix/hardware-configuration.nix
+++ host/acervnix/hardware-configuration.nix
@@ -1,5 +1,5 @@
 { lib, config, ... }: {
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "sr_mod" "rtsx_usb_sdmmc" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "sd_mod" "sr_mod" "rtsx_usb_sdmmc" ];
   boot.initrd.kernelModules = [];
   boot.kernelModules = [ "kvm_intel" ];
   boot.extraModulePackages = [];
