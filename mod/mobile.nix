3478c49b985215f7a277d5cecc6e90998540236d
Flameopathic <flameopathic@gmail.com>
2024-06-26 23:09:53 -0400

    fix: syntax

--- mod/mobile.nix
+++ mod/mobile.nix
@@ -2,8 +2,10 @@
   imports = [
     ./battery.nix
   ];
-  config.security.pki.certificateFiles = [ ../resources/enterprise.pem ];
-  systemd.tmpfiles.rules = [
-    "z /sys/class/backlight/intel_backlight/brightness 0664 root wheel" # allows brillo to change brightness
-  ];
+  config = {
+    security.pki.certificateFiles = [ ../resources/enterprise.pem ];
+    systemd.tmpfiles.rules = [
+      "z /sys/class/backlight/intel_backlight/brightness 0664 root wheel" # allows brillo to change brightness
+    ];
+  };
 }
