8811a13d2d62fdc58190f13e8af3b2c20cce5889
Flameopathic <flameopathic@gmail.com>
2024-07-03 23:02:58 -0400

    steam-run for shaktop

--- host/shaktop/configuration.nix
+++ host/shaktop/configuration.nix
@@ -1,6 +1,8 @@
 { pkgs, ... }: {
-  environment.systemPackages = [ pkgs.python3 ];
+  environment.systemPackages = [ pkgs.python3 pkgs.steam-run ];
   
+
+
   system.stateVersion = "23.11"; # Did you read the comment?
 }
 
