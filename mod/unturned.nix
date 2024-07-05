a04f1b3fb3a8ede672aaf8ae0763f07e82a9d3a0
Flameopathic <flameopathic@gmail.com>
2024-07-04 22:24:02 -0400

    remove port forwarding

--- mod/unturned.nix
+++ mod/unturned.nix
@@ -1,5 +1,3 @@
 { pkgs, ... }: { # basic utilities and configuration to set up an unturned server
   environment.systemPackages = [ pkgs.steam-run pkgs.steamcmd ];
-  networking.firewall.allowedUDPPorts = [ 27015 27016 ];
-  networking.firewall.allowedTCPPorts = [ 27015 27016 ];
 }
