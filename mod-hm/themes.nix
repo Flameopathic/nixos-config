605f1ebd3fdb323df02791b9f845ea19d41f0962
Flameopathic <flameopathic@gmail.com>
2024-07-15 08:28:51 -0400

    moved main theming to themes.nix; consider moving all theming

---
+++ mod-hm/themes.nix
@@ -0,0 +1,69 @@
+{ config, pkgs, lib, inputs, ... }: {
+  imports = [
+    inputs.nix-colors.homeManagerModules.default
+  ];
+  config = {
+    colorScheme = lib.mkDefault inputs.nix-colors.colorSchemes.rose-pine-moon;
+
+    home.packages = with pkgs; [
+      (writeShellApplication { # credit: Janik-Haag
+        name = "toggle-theme";
+        runtimeInputs = with pkgs; [ home-manager coreutils ripgrep ];
+        text = ''
+          "$(home-manager generations | head -1 | rg -o '/[^ ]*')"/specialisation/light/activate && hyprctl reload
+        '';
+      })
+    ];
+
+    home.pointerCursor = {
+      gtk.enable = true;
+      package = pkgs.bibata-cursors-translucent;
+      name = "Bibata_Ghost";
+      size = config.flame.ui.cursorSize;
+    };
+
+    gtk = {
+			enable = true;
+			cursorTheme = {
+				name = "Bibata_Ghost";
+				package = pkgs.bibata-cursors-translucent;
+				size = config.flame.ui.cursorSize;
+			};
+			iconTheme = {
+				name = config.gtk.theme.name;
+				package = pkgs.rose-pine-icon-theme;
+			};
+			theme = {
+				name = lib.mkDefault "rose-pine-moon";
+				package = pkgs.rose-pine-gtk-theme;
+			};
+		};
+
+    xdg.desktopEntries.theme-switch = {
+      name = "Toggle theme";
+      exec = "toggle-theme";
+      categories = [ "Utility" ];
+    };
+
+    specialisation.light.configuration = {
+			colorScheme = inputs.nix-colors.colorSchemes.rose-pine-dawn;
+
+			programs.bash.shellAliases.snrbs = "sudo nixos-rebuild switch && toggle-theme";
+
+			home.packages = with pkgs; [ # credit: Janik-Haag 
+				(hiPrio (writeShellApplication {
+				name = "toggle-theme";
+				runtimeInputs = with pkgs; [ home-manager coreutils ripgrep ];
+				text =
+					''
+						"$(home-manager generations | head -2 | tail -1 | rg -o '/[^ ]*')"/activate && hyprctl reload
+					'';
+				}))
+			];
+
+      gtk.theme.name = "rose-pine-dawn";
+
+			flame.hyprland.wallpaper = "lwp.png";
+		};
+  };
+}
--- mod-hm/ui-apps.nix
+++ mod-hm/ui-apps.nix
@@ -1,11 +1,8 @@
-{ config, pkgs, inputs, lib, ... }: 
-
-let cfg = config.flame.ui;
-in {
+{ config, pkgs, lib, ... }: {
 	imports = [
 		./firefox.nix
 		./vscode.nix
-		inputs.nix-colors.homeManagerModules.default
+		./themes.nix
 	];
 
 	options.flame.ui.cursorSize = lib.mkOption {
@@ -13,7 +10,6 @@ in {
 	};
 
 	config = {
-		colorScheme = lib.mkDefault inputs.nix-colors.colorSchemes.rose-pine-moon;
 		home.packages = with pkgs; [
 			nerdfonts
 			obsidian
@@ -27,46 +23,13 @@ in {
 			# fm # really good looking file manager, gonna wait until it's a bit better on the back end
 			pcmanfm
 			bottles
-			(writeShellApplication { # credit: Janik-Haag
-        name = "toggle-theme";
-        runtimeInputs = with pkgs; [ home-manager coreutils ripgrep ];
-        text =
-          ''
-            "$(home-manager generations | head -1 | rg -o '/[^ ]*')"/specialisation/light/activate && hyprctl reload
-          '';
-      })
 			geeqie
 			vesktop # TODO: change to `programs.vesktop` when home manager merges the pr
 		];
-		programs = {
-			kitty = {
-				enable = true;
-				theme = config.colorScheme.name;
-			};
-		};
 
-		home.pointerCursor = {
-			gtk.enable = true;
-			package = pkgs.bibata-cursors-translucent;
-			name = "Bibata_Ghost";
-			size = cfg.cursorSize;
-		};
-		
-		gtk = {
+		programs.kitty = {
 			enable = true;
-			cursorTheme = {
-				name = "Bibata_Ghost";
-				package = pkgs.bibata-cursors-translucent;
-				size = cfg.cursorSize;
-			};
-			iconTheme = {
-				name = if config.colorScheme.name == "Rosé Pine Dawn" then "rose-pine-dawn" else if config.colorScheme.name == "Rosé Pine Moon" then "rose-pine-moon" else "";
-				package = pkgs.rose-pine-icon-theme;
-			};
-			theme = {
-				name = if config.colorScheme.name == "Rosé Pine Dawn" then "rose-pine-dawn" else if config.colorScheme.name == "Rosé Pine Moon" then "rose-pine-moon" else "";
-				package = pkgs.rose-pine-gtk-theme;
-			};
+			theme = config.colorScheme.name;
 		};
 
 		xdg.desktopEntries = {
@@ -81,27 +44,6 @@ in {
         exec = "reboot";
         categories = [ "Utility" ];
       };
-      theme-switch = lib.mkDefault {
-        name = "Toggle theme";
-        exec = "toggle-theme";
-        categories = [ "Utility" ];
-      };
     };
-
-		specialisation.light.configuration = {
-			colorScheme = inputs.nix-colors.colorSchemes.rose-pine-dawn;
-			programs.bash.shellAliases.snrbs = "sudo nixos-rebuild switch && toggle-theme";
-			home.packages = with pkgs; [ # credit: Janik-Haag 
-				(hiPrio (writeShellApplication {
-				name = "toggle-theme";
-				runtimeInputs = with pkgs; [ home-manager coreutils ripgrep ];
-				text =
-					''
-						"$(home-manager generations | head -2 | tail -1 | rg -o '/[^ ]*')"/activate && hyprctl reload
-					'';
-				}))
-			];
-			flame.hyprland.wallpaper = "lwp.png";
-		};
 	};
 }
