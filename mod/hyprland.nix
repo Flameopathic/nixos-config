9d81b54d819e7bc03ec0d356bde67b783cc93047
Flameopathic <flameopathic@gmail.com>
2024-08-11 21:00:58 -0400

    Revert "swaylock again!"
    This reverts commit 55e11081efe0b165f65f65ef692726bc4a24dc08.

--- mod/hyprland.nix
+++ mod/hyprland.nix
@@ -1,7 +1,7 @@
 { ... }: {
   config = {
     programs.hyprland.enable = true;
-    security.pam.services.swaylock = { };
+    security.pam.services.hyprlock = { };
     home-manager.users.flame.imports = [
       ../mod-hm/hyprland.nix
     ];
