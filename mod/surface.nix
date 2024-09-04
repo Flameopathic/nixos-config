6c79813878497c6407bf661f39d554ea5a8c83f0
Flameopathic <flameopathic@gmail.com>
2024-09-04 09:29:50 -0400

    feat: surface msr-tools

--- mod/surface.nix
+++ mod/surface.nix
@@ -1,6 +1,7 @@
-{ inputs, ... }: {
+{ inputs, pkgs, ... }: {
   imports = [
     inputs.nixos-hardware.nixosModules.microsoft-surface-pro-intel
     ./mobile.nix
   ];
+  config.environment.systemPackages = [ pkgs.msr-tools ];
 }
