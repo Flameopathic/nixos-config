3d37f8a93febb2838233285506701a3f28d54e9e
Flameopathic <flameopathic@gmail.com>
2024-09-21 20:57:23 -0400

    surface patchwork slowdown solution

--- mod/surface.nix
+++ mod/surface.nix
@@ -3,5 +3,15 @@
     inputs.nixos-hardware.nixosModules.microsoft-surface-pro-intel
     ./mobile.nix
   ];
-  config.environment.systemPackages = [ pkgs.msr-tools ];
+  config.environment.systemPackages = [
+    pkgs.msr-tools
+    ( pkgs.writeShellApplication {
+        # credit: Janik-Haag
+        name = "surface-msr-adjust";
+        runtimeInputs = with pkgs; [ coreutils kitty msr-tools ];
+        text = ''
+          sudo -A modprobe msr; sudo wrmsr --all 0x601 0x408
+        '';
+      })
+  ];
 }
