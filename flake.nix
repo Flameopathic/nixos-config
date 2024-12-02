b015c36963b73fd67c815481520c395c1d00e0aa
Flameopathic <flameopathic@gmail.com>
2024-12-02 18:57:12 -0500

    update to 24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "my flkae <3";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     nixos-hardware.url = "github:nixos/nixos-hardware";
     home-manager = {
