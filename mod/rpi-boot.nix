6c3a2379da90f056452a9d2c9ea5d983b2e623be
Flameopathic <flameopathic@gmail.com>
2024-12-06 15:20:03 -0500

    rpi boot fix

--- mod/rpi-boot.nix
+++ mod/rpi-boot.nix
@@ -1,28 +1,12 @@
 {
-  config,
   pkgs,
-  lib,
   ...
 }:
-let
-  cfg = config.flame.rpi-boot;
-in
 {
-  options.flame.rpi-boot.overclock = lib.mkEnableOption "overclock, set up for pi 4";
 
   config = {
     boot.loader.grub.enable = false;
     boot.loader.generic-extlinux-compatible.enable = true;
     environment.systemPackages = [ pkgs.libraspberrypi ];
-
-    boot.loader.raspberryPi.firmwareConfig =
-      if cfg.overclock then
-        ''
-            arm_freq=2000
-          	over_voltage=6
-          	gpu_freq=750
-        ''
-      else
-        "";
   };
 }
