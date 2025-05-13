26f468d02e23dd7375988716b8a3e12457f4b45d
Flameopathic <flameopathic@gmail.com>
2025-05-13 18:21:05 -0400

    battery tweaks

--- mod/battery.nix
+++ mod/battery.nix
@@ -1,15 +1,23 @@
 { ... }:
 {
   config = {
-    services.tlp = {
-      enable = true;
-      settings = {
-        CPU_BOOST_ON_AC = 1;
-        CPU_BOOST_ON_BAT = 0;
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    services = {
+      thermald.enable = true;
+      tlp = {
+        enable = true;
+        settings = {
+          CPU_BOOST_ON_AC = 1;
+          CPU_BOOST_ON_BAT = 0;
+          CPU_SCALING_GOVERNOR_ON_AC = "performance";
+          CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+          # only works on lenovo
+          START_CHARGE_THRESH_BAT0 = 80;
+          STOP_CHARGE_THRESH_BAT0 = 85;
+        };
       };
     };
-    powerManagement.cpuFreqGovernor = "schedutil";
+
+    powerManagement.powertop.enable = true;
   };
 }
