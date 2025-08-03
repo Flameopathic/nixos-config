26dbb1d88a55470c871a990eb78fd61ff6903a71
Flameopathic <flameopathic@gmail.com>
2025-08-02 22:58:12 -0400

    stylix: remove home-manager version pin

--- flake.nix
+++ flake.nix
@@ -51,7 +51,6 @@
       url = "github:nix-community/stylix/release-25.05";
       inputs = {
         nixpkgs.follows = "nixpkgs";
-        home-manager.follows = "home-manager";
       };
     };
     spicetify-nix = {
