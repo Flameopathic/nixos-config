c40ed13489c201fd55c6bed98d5588b3018dff43
Flameopathic <flameopathic@gmail.com>
2025-11-20 14:30:08 -0600

    fnix2: open spotify on startup

--- host/fnix2/home.nix
+++ host/fnix2/home.nix
@@ -1,7 +1,11 @@
+{ lib, ... }:
 {
   home.sessionVariables = {
     GDK_SCALE = 2;
   };
-  
+
+  wayland.windowManager.hyprland.settings.exec-once =
+    lib.mkForce "swaylock & swww-daemon & mako & nm-applet & pasystray & spotify";
+
   home.stateVersion = "23.11"; # home manager can be updated without changing this - read documentation
 }
