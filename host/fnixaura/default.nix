9970ac419f4c95d06a37ae4eba5790ca033a8e09
Flameopathic <flameopathic@gmail.com>
2025-12-30 14:40:14 -0500

    fnixaura: use kernel 6.17 for audio

--- host/fnixaura/default.nix
+++ host/fnixaura/default.nix
@@ -10,20 +10,14 @@
     ../../mod/syncthing.nix
     ../../mod/rose-pine.nix
     ../../mod/gaming.nix
-    {
-      # https://codeberg.org/AndrewKvalheim/configuration/src/commit/9b0b0f90a2220c635f67123d435d239182f73406/hosts/main/system.nix#L24-L32
-      systemd.services.configure-sound-leds = rec {
-        wantedBy = [ "sound.target" ];
-        after = wantedBy;
-        serviceConfig.Type = "oneshot";
-        script = ''
-          echo follow-route > /sys/class/sound/ctl-led/mic/mode
-          echo off > /sys/class/sound/ctl-led/speaker/mode # follow-route pending https://discourse.nixos.org/t/20480
-        '';
-      };
+    (
+      { pkgs, ... }:
+      {
+        boot.kernelPackages = pkgs.linuxPackages_6_17;
 
-      system.stateVersion = "24.11";
-    }
+        system.stateVersion = "24.11";
+      }
+    )
   ];
   home-modules = [
     ../../mod-hm/ui-apps.nix
