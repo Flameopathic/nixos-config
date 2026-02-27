baa3e1f927a34aff5a613eea86cb53fb15e86bfb
Flameopathic <flameopathic@gmail.com>
2026-02-27 11:32:21 -0600

    mobile: remove simplify hibernation

--- mod/hardware/mobile/default.nix
+++ mod/hardware/mobile/default.nix
@@ -1,13 +1,15 @@
 {
   imports = [
     # ./battery.nix
-    ./suspend-then-hibernate.nix
   ];
   config = {
     systemd = {
       tmpfiles.rules = [
         "z /sys/class/backlight/intel_backlight/brightness 0664 root wheel" # allows brillo to change brightness
       ];
+      sleep.extraConfig = ''
+        HibernateDelaySec=15m
+      '';
     };
   };
 }
